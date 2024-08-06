798aa182bdc1d36ce4013bf1f053fc41725c0e60
Nkwor-Jane <nkwor.jane.chinelo@gmail.com>
2024-08-07 00:16:11 +0100

    A stopwatch

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Stopwatch from './components/Stopwatch'
+
+function App() {
+
+  return (
+    <>
+      <Stopwatch/> 
+    </>
+  )
+}
+
+export default App
